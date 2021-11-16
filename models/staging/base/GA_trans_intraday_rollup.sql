e60d9e08b4cf4f1353bcfde35e1c6f3ca7e59324
Fletcher Herman <fletcher.herman@cottonon.com.au>
2021-11-16 01:56:26 +0000

    include campaign_templates

---
+++ models/staging/base/GA_trans_intraday_rollup.sql
@@ -0,0 +1,15 @@
+SELECT
+    TIMESTAMP_SECONDS(visitStartTime) as aest_datetime
+    ,CONCAT(fullVisitorId, CAST(visitStartTime AS STRING)) AS session_id
+    ,SPLIT(hits.page.pagePath, '/')[SAFE_OFFSET(1)] as site_region 
+    ,MAX((totals.totalTransactionRevenue / 1000000)) as transactionRevenue_AUD
+    ,MAX((cast(hits.transaction.transactionTax as INT64) / 1000000)) as transactionTax_AUD
+    ,MAX((cast(hits.transaction.transactionShipping as INT64) / 1000000)) as transactionShipping_AUD
+    ,MAX((cast(hits.transaction.localTransactionRevenue as INT64) / 1000000)) as transactionRevenue_local
+    ,MAX((cast(hits.transaction.localTransactionTax as INT64) / 1000000)) as transactionTax_local
+    ,MAX((cast(hits.transaction.localTransactionShipping as INT64) / 1000000)) as transactionShipping_local
+FROM {{ source('132581016', 'ga_sessions_intraday_*')}}, UNNEST (hits) AS hits
+WHERE _TABLE_SUFFIX BETWEEN FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 750 DAY)) 
+    AND FORMAT_DATE('%Y%m%d',DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)) 
+    AND hits.transaction.transactionId IS NOT NULL
+GROUP BY 1,2,3    
