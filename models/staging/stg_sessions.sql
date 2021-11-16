ddd4ac4afd0bb010e413a379ae1b283d710a5254
Fletcher Herman <fletcher.herman@cottonon.com.au>
2021-11-16 01:23:14 +0000

    clean up

--- models/staging/stg_sessions.sql
+++ models/staging/stg_sessions.sql
@@ -5,16 +5,16 @@ SELECT
     ,hostname
     ,pagepath
     ,fullURL
-    ,revenue_AUD
+    ,datasource
+    ,type
     ,COUNT(*) AS pageviews
     ,COUNT(DISTINCT fullURL) OVER(PARTITION BY session_id) AS unique_pageviews_within_session
     ,1 as unique_pageviews
   FROM (
         SELECT * 
         FROM {{ ref('GA_hits_rollup') }}
-        WHERE type = "PAGE" 
   )
   GROUP BY
-    session_id, site_region, hostname, pagePath, revenue_AUD, fullURL, aest_datetime 
+    session_id, site_region, hostname, pagePath, revenue_AUD, fullURL, aest_datetime , datasource, type
   ORDER BY
     pageviews DESC
