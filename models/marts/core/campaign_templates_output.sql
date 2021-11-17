33ab48e56d1e430025bb385faafb267ffbea206f
Fletcher Herman <fletcher.herman@cottonon.com.au>
2021-11-17 01:08:52 +0000

    materialized as table and clean trans rollup

--- models/marts/core/campaign_templates_output.sql
+++ models/marts/core/campaign_templates_output.sql
@@ -11,12 +11,15 @@ select
         when site_region='uk' then DATETIME(aest_datetime, "Europe/London")  
         when site_region='za' then DATETIME(aest_datetime, "Africa/Johannesburg")                                   
         else null end) as date
+    ,dataSource
     ,channelGrouping
-    ,count((distinct session_id)) as disc_sessions
+    ,site_region
+    ,user_region
+    ,count((session_id)) as disc_sessions
     ,SUM(IF(revenue_AUD is not null,1,0)) as transactions
     ,SUM(IF(revenue_AUD is not null,1,0)) / count((session_id)) as conversion_rate
     ,SUM(revenue_AUD) as revenue
     ,SUM(revenue_AUD) / SUM(IF(revenue_AUD is not null,1,0)) as AOV
 from {{ ref('int_campaign_templates')}} 
-group by 1,2,3 
+group by 1,2,3,4,5,6 
 order by 1 desc
--- models/marts/templates/int_campaign_templates.sql
+++ models/marts/templates/int_campaign_templates.sql
@@ -4,10 +4,12 @@ SELECT
     ,EXTRACT(DATE FROM h.aest_datetime) as date
     ,h.site_region
     ,h.user_region
-    ,channelGrouping
+    ,h.dataSource
+    ,h.channelGrouping
     ,h.session_id
     ,(transactionRevenue_AUD) - (transactionTax_AUD + transactionShipping_AUD) as revenue_AUD
 FROM {{ ref('GA_hits_rollup') }} h
     LEFT JOIN {{ ref('GA_trans_rollup')}} 
-    USING (session_id)
-WHERE h.site_region IN ('au','nz','za','us','my','sg','hc','uk')
+    USING (unique_visit_id)
+WHERE h.hit_number = 1    
+--WHERE h.site_region IN ('au','nz','za','us','my','sg','hc','uk')
